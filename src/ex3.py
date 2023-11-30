d489447d484575d40c8b43498386e38b6b330fed
Drew-Huth <drew.huth@fiserv.com>
2023-11-30 15:45:43 -0500

    1-3 and Cleanup

--- src/ex3.py
+++ src/ex3.py
@@ -5,10 +5,13 @@ def create_files(file_name):
     count = 0
     biglist = []
     lillist = [] 
-    with open(f"files/{file_name}", "r") as file:
-        for line in file:
+    bigwords = open("files\large-words.txt", "w")
+    smallwords = open("files\small-words.txt", "w")
+    wordstxt = open("files/words.txt", "r")
+    
+    with wordstxt as file_input:
+        for line in file_input:
             new_string = re.sub(r'[^\w\s]', '', line)
-            
             for word in new_string.split():
                 if len(word) >=3 and word not in biglist :
                     biglist.append(word)
@@ -17,12 +20,12 @@ def create_files(file_name):
                 else: 
                     continue
     
-    with open("files\large-words.txt", "w") as large_output:
+    with bigwords as large_output:
         for word in biglist:
             large_output.writelines(word + "\n")
             count += 1
                 
-    with open("files\small-words.txt", "w") as small_output:
+    with smallwords as small_output:
         for word in lillist:
             small_output.writelines(word + "\n")
             count += 1
