2f93eacd8844e4fd154e3027c38eaccd554f7f9a
Drew-Huth <drew.huth@fiserv.com>
2023-12-01 11:35:42 -0500

    Completed

--- src/ex5.py
+++ src/ex5.py
@@ -1,9 +1,58 @@
 from pprint import pprint
+import csv
+import re
+
+def Convert (tuples, dicts):
+    dicts = dict(tuples)
+    return dicts
 
 def build_car_list():
-    pass # TODO
+    
+    miles = []
+    makes = []
+    ids = []
+    both = []
+    
+    with open("files/input.txt") as file_input:
+        file_reader = csv.reader(file_input)
+        next(file_reader, None)
+        for row in file_reader:
+            num = row[1]
+            if '.' in num:
+                continue
+            num = int(num)
+            miles.append(num)
+        
+    with open("files/car-ids.txt") as file_input:
+        file_reader = csv.reader(file_input)
+        for row in file_reader:
+            if row[0] == '4':
+                continue
+            car_ids = int(row[0])
+            ids.append(car_ids)
+        
+    with open("files/car-ids.txt") as file_input:
+        file_reader = csv.reader(file_input)
+        for row in file_reader:
+            if row[0] == '4':
+                continue
+            word = row[1].strip()
+            makes.append(word)
 
+    count = 0
+    while count != len(ids):
+        car = {'id': ids[count],
+               'miles': miles[count],
+               'model': makes[count]}
+        both.append(car)
+        count += 1
 
+        
+    return both
+
+    
+
+                
 def ex5():
     car_list = build_car_list()
     pprint(car_list)
